6bc41f33d8d5e58c4c5c4b4acc217763df5bc36e
Ethan <ethan.mathieu@yale.edu>
2023-06-26 00:15:46 -0400

    all but testing done

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Button, TextField } from "@mui/material";
+import { DesktopDatePicker , LocalizationProvider} from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 
 class AddTodo extends Component {
   // Create a local react state of the this component with both content date property set to nothing.
@@ -7,9 +9,16 @@ class AddTodo extends Component {
     super();
     this.state = {
       content: "",
-      date: ""
+      date: "",
+      due: null
     };
   }
+
+  newHandleChange = (event) => {
+    // Update the state with the selected date
+    this.setState({ due: event});
+  };
+
   // The handleChange function updates the react state with the new input value provided from the user and the current date/time.
   // "event" is the defined action a user takes. In this case, the event is triggered when the user types something
   // into the text field.
@@ -29,7 +38,8 @@ class AddTodo extends Component {
       this.props.addTodo(this.state);
       this.setState({
         content: "",
-        date: ""
+        date: "",
+        due: null
       });
     }
   };
@@ -49,6 +59,15 @@ class AddTodo extends Component {
           onChange={this.handleChange}
           value={this.state.content}
         />
+        <LocalizationProvider dateAdapter={AdapterDateFns}>         
+          <DesktopDatePicker
+              id="new-item-date"
+              label="Due Date"
+              value={this.state.due}
+              onChange={this.newHandleChange}
+              renderInput={(params) => <TextField {...params} />}
+          />
+        </LocalizationProvider>
         <Button
           style={{ marginLeft: "10px" }}
           onClick={this.handleSubmit}
@@ -57,6 +76,7 @@ class AddTodo extends Component {
         >
           Add
         </Button>
+   
       </div>
     );
   }
--- src/component/todos.js
+++ src/component/todos.js
@@ -12,9 +12,17 @@ import { Card, Grid, ListItemButton, ListItemText, Checkbox} from "@mui/material
 const Todos = ({ todos, deleteTodo }) => {
   const todoList = todos.length ? (
     todos.map((todo) => {
+
+      let color = "white"
+
+      let cur_date = new Date()
+      if (cur_date.getTime() > todo.due.getTime()) {
+        color = "red"
+      }
+
       return (
         <Grid key={todo.id}>
-          <Card style={{marginTop:10}}>
+          <Card style={{marginTop:10, backgroundColor: color}}>
             {/* Remember, we set the local state of this todo item when the user submits the form in 
             AddTodo.js. All we need to do is return the todo list item {todo.content} as well as its 
             current date/time {todo.date}. Also, the item's id is utilized in order to correctly delete an item from the Todo list*/}.
